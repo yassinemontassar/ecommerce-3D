bc86d3b5ffac86becb3bcf7b943092cd16954928
yassinemontassar <sawthegamer70@gmail.com>
2023-12-01 23:41:52 +0100

    fixes

--- src/app/canvas/Shirt.jsx
+++ src/app/canvas/Shirt.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react'
 import { easing } from 'maath'
 import { useSnapshot } from 'valtio'
--- src/app/hooks/colors.js
+++ src/app/hooks/colors.js
@@ -1,26 +1,40 @@
-// useColor.js
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useColor = () => {
+  // Check if we are running on the client side (browser)
+  const isClient = typeof window !== 'undefined';
+
   // Retrieve the initial color from local storage or provide a default color
-  const initialColor = localStorage.getItem('selectedColor') || '#EFBD48';
+  const initialColor = isClient ? localStorage.getItem('selectedColor') || '#EFBD48' : '#EFBD48';
   const [color, setColor] = useState(initialColor);
 
   const addColor = (newColor) => {
     // Update the color state
     setColor(newColor);
 
-    // Save the color hex value to local storage
-    localStorage.setItem('selectedColor', newColor);
+    // Save the color hex value to local storage (if running on the client side)
+    if (isClient) {
+      localStorage.setItem('selectedColor', newColor);
+    }
   };
 
   const getColor = () => {
-    // Retrieve the current color from local storage
-    const storedColor = localStorage.getItem('selectedColor');
+    // Retrieve the current color from local storage (if running on the client side)
+    const storedColor = isClient ? localStorage.getItem('selectedColor') : null;
 
     // Return the stored color or a default color if not found
     return storedColor || '#EFBD48';
   };
+
+  // Use useEffect to initialize the color on the client side
+  useEffect(() => {
+    if (isClient) {
+      const storedColor = localStorage.getItem('selectedColor');
+      if (storedColor) {
+        setColor(storedColor);
+      }
+    }
+  }, [isClient]);
 
   return { color, addColor, getColor };
 };
